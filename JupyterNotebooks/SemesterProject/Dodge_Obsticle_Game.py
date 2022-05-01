be1b51a0e6ae8af6cf5c37ee9fe22cdc5217d774
RyanJones2002 <ryan.jones2@marist.edu>
2022-05-01 18:21:07 -0400

    Project

--- JupyterNotebooks/SemesterProject/Dodge_Obsticle_Game.py
+++ JupyterNotebooks/SemesterProject/Dodge_Obsticle_Game.py
@@ -1,195 +1,323 @@
 
 
-
-# Need Exit Function
-# Need player class, 
-    #move function
-    #Is hit function
-    #   
-# object class(ball),
-    #random spawn function
-    #move function
-    #destroy function
-    #move all: takes a list of all balls moves them all
-#  list of objects of balls to keep track, 
-
-
-from turtle import position
 import pygame
+from pygame.locals import *
 import time
 import random
 
+#Window size & Variables
+WIDTH = 400
+HEIGHT = 500
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
+SPEED = 5
+SCORE = 0
 
-# Window size
-window_x = 400
-window_y = 500
-
-# defining colors
+#Defining colors
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 magenta = pygame.Color(255, 0, 255)
 yellow = pygame.Color(255, 255, 0)
 cyan = pygame.Color(0, 255, 255)
+blue = pygame.Color(0, 0, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
 
-# Initialising pygame
+#Initialising pygame
 pygame.init()
+vec = pygame.math.Vector2
 
-# Initialise game window
+#Initialise game window & setting caption
 pygame.display.set_caption('Dodge Obsticle Game')
-game_window = pygame.display.set_mode((window_x, window_y))
+game_window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# FPS (frames per second) controller
-fps = pygame.time.Clock()
+#FPS (frames per second) controller
+FramePerSec = pygame.time.Clock()
 
-# defining player default position
-player_position = [200, 450]
-
-# defining player speed?
-player_speed = 15
-
-
-
-# obsticle position
-obsticle_position = [random.randrange(1, (window_x//10)) * 10,
-				random.randrange(1, (window_y//10)) * 10]
-
-
-
-obsticle_spawn = True
-
-# initial score
-score = 0
-
-# displaying Score function
+#Displaying Score function
 def show_score(choice, color, font, size):
 
-	# creating font object score_font
+	#Creating font object score_font
 	score_font = pygame.font.SysFont(font, size)
 	
-	# create the display surface object
-	# score_surface
-	score_surface = score_font.render('Score : ' + str(score), True, color)
+	#Create the display surface object
+	score_surface = score_font.render('Score : ' + str(SCORE), True, color)
 	
-	# create a rectangular object for the text
-	# surface object
+	#Create a rectangular object for the text surface object
 	score_rect = score_surface.get_rect()
 	
-	# displaying text
+	#Displaying text
 	game_window.blit(score_surface, score_rect)
 
-# game over function
+#Defining the player 
+class Player(pygame.sprite.Sprite):
+
+    #Initilizing player attributes
+    def __init__(self):
+
+        #Super function allows us to avoid using base class name explicitly
+        super().__init__()
+
+        #Adding size
+        self.surf = pygame.Surface((25,25))
+
+        #Adding color
+        self.surf.fill((magenta))
+
+        #Defining shape & center
+        self.rect = self.surf.get_rect(center = (200, 450))
+
+        #Defining starting position
+        self.pos = vec((200, 450))
+
+        #Defining starting velocity
+        self.vel = vec(0,0)
+
+        #Defining starting acceleration
+        self.acc = vec(0,0)
+
+    #creating player movement
+    def move(self):
+
+        #Defining starting acceleration
+        self.acc = vec(0,0)
+
+        #When L/R arrow key pressed, -/+ acceleration in corresponding direction
+        pressed_keys = pygame.key.get_pressed() 
+        if pressed_keys[K_LEFT]:
+            self.acc.x = -ACC
+        if pressed_keys[K_RIGHT]:
+            self.acc.x = ACC  
+
+        #Calculating new position based on key pressed with respect to velocity, friction, & acceleration
+        self.acc.x += self.vel.x * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + .5 * self.acc
+
+        #Wrap around screen feature
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+
+        #returns the point at the center of the bottom of the rectangle
+        self.rect.midbottom = self.pos
+
+#Defining the first object
+class Object(pygame.sprite.Sprite):
+
+      #Initilizing objects attributes
+      def __init__(self):
+
+        #Super function allows us to avoid using base class name explicitly
+        super().__init__() 
+
+        #Adding size
+        self.surf = pygame.Surface((25,25))
+
+        #Adding color
+        self.surf.fill((cyan)) 
+
+        #Get rectangle object
+        self.rect = self.surf.get_rect()
+
+        #Making the center of the object spawn randomly along top of display
+        self.rect.center=(random.randint(40,WIDTH-40),0)
+
+      #Creating object movement  
+      def move(self):
+
+        #Moves object down 10 pixels
+        self.rect.move_ip(0,10)
+
+        #Allows you to modify variable (score) outside of current scope 
+        global SCORE
+
+        #Checks if top of object has reached end of screen and if true, resets at random location along x-axis
+        if (self.rect.top > 500):
+            self.rect.top = 0
+            self.rect.center = (random.randint(30, 370), 0)
+
+            #Increase score when object passes
+            SCORE += 5 
+
+      #Blit() takes 2 inputs (surface & object) & draws rectangle to image (the surface)
+      def draw(self, surface):
+        surface.blit(self.image, self.rect) 
+
+#Definations are constant for remaining objects
+
+#Defining the 2nd object
+class Object_2(pygame.sprite.Sprite):
+      def __init__(self):
+        super().__init__() 
+        self.surf = pygame.Surface((30,30))
+        self.surf.fill((yellow)) 
+        self.rect = self.surf.get_rect()
+        self.rect.center=(random.randint(40,WIDTH-40),0) 
+      def move(self):
+        self.rect.move_ip(0,12)
+        global SCORE
+        if (self.rect.top > 500):
+            self.rect.top = 0
+            self.rect.center = (random.randint(30, 370), 0)
+            SCORE += 5 
+            
+        
+      def draw(self, surface):
+        surface.blit(self.image, self.rect) 
+
+#Defining the 3rd object
+class Object_3(pygame.sprite.Sprite):
+      def __init__(self):
+        super().__init__() 
+        self.surf = pygame.Surface((30,30))
+        self.surf.fill((blue)) 
+        self.rect = self.surf.get_rect()
+        self.rect.center=(random.randint(40,WIDTH-40),0) 
+      def move(self):
+        self.rect.move_ip(0,8)
+        global SCORE
+        if (self.rect.top > 500):
+            self.rect.top = 0
+            self.rect.center = (random.randint(30, 370), 0)
+            SCORE += 5 
+            
+      def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+#Defining the 4th object
+class Object_4(pygame.sprite.Sprite):
+      def __init__(self):
+        super().__init__() 
+        self.surf = pygame.Surface((40,40))
+        self.surf.fill((red)) 
+        self.rect = self.surf.get_rect()
+        self.rect.center=(random.randint(40,WIDTH-40),0) 
+      def move(self):
+        self.rect.move_ip(0,5)
+        global SCORE
+        if (self.rect.top > 500):
+            self.rect.top = 0
+            self.rect.center = (random.randint(30, 370), 0)
+            SCORE += 5 
+            
+      def draw(self, surface):
+        surface.blit(self.image, self.rect) 
+
+#Defining the 5th object
+class Object_5(pygame.sprite.Sprite):
+      def __init__(self):
+        super().__init__() 
+        self.surf = pygame.Surface((35,35))
+        self.surf.fill((green))
+        self.rect = self.surf.get_rect()
+        self.rect.center=(random.randint(40,WIDTH-40),0) 
+      def move(self):
+        self.rect.move_ip(0,10)
+        global SCORE
+        if (self.rect.top > 500):
+            self.rect.top = 0
+            self.rect.center = (random.randint(30, 370), 0)
+            SCORE += 5 
+            
+      def draw(self, surface):
+        surface.blit(self.image, self.rect) 
+
+#Game over function
 def game_over():
 
-	# creating font object my_font
+	#Creating font object my_font
 	my_font = pygame.font.SysFont('times new roman', 50)
 	
-	# creating a text surface on which text
-	# will be drawn
+	#Creating a text surface on which text will be drawn
 	game_over_surface = my_font.render(
-		'Your Score is : ' + str(score), True, magenta)
+		'Your Score Is : ' + str(SCORE), True, magenta)
 	
-	# create a rectangular object for the text
-	# surface object
+	#Create a rectangular object for the text surface object
 	game_over_rect = game_over_surface.get_rect()
 	
-	# setting position of the text
-	game_over_rect.midtop = (window_x/2, window_y/4)
+	#Setting position of the text
+	game_over_rect.midtop = (WIDTH/2, HEIGHT/4)
 	
-	# blit will draw the text on screen
+	#Blit will draw the text on screen
 	game_window.blit(game_over_surface, game_over_rect)
 	pygame.display.flip()
 	
-	# after 2 seconds we will quit the program
-	time.sleep(2)
+	#After 3 seconds we will quit the program
+	time.sleep(3)
 	
-	# deactivating pygame library
+	#Deactivating pygame library
 	pygame.quit()
 	
-	# quit the program
+	#Quit the program
 	quit()
 
-class Player:
-    def __init__(self):
-        self.positionX = 200
-        self.positionY = 450
-        self.rect = pygame.Rect(self.positionX,self.positionY,50,50)
 
-    def getX(self):
-        return self.positionX
-    
-    def getY(self):
-        return self.positionY
+#Defining the player sprite
+P1 = Player() 
 
-    def move(self):
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    change_to = 'LEFT'
-                    del self.rect
-                    self.positionX -= 5
-                    self.rect = pygame.Rect(self.positionX,self.positionY,50,50)
-                    print("left", self.positionX)
-                    self.render() 
-                if event.key == pygame.K_RIGHT:
-                    change_to = 'RIGHT'
-                    del self.rect
-                    self.positionX += 5
-                    self.rect = pygame.Rect(self.positionX,self.positionY,50,50)
-                    print("right", self.positionX)    
-                    self.render()  
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-                    change_to = 'LEFT'
-                    del self.rect
-                    self.positionX -= 5
-                    self.rect = pygame.Rect(self.positionX,self.positionY,50,50)
-                    print("left", self.positionX)
-                    self.render() 
-                if event.key == pygame.K_RIGHT:
-                    change_to = 'RIGHT'
-                    del self.rect
-                    self.positionX += 5
-                    self.rect = pygame.Rect(self.positionX,self.positionY,50,50)
-                    print("right", self.positionX)    
-                    self.render()  
+#Defining the object sprites
+O1 = Object()
+O2 = Object_2()
+O3 = Object_3()
+O4 = Object_4()
+O5 = Object_5()
 
+#Grouping the object sprites
+objects = pygame.sprite.Group()
+#Adding objects to group
+objects.add(O1, O2, O3, O4, O5)
 
-    def render(self):
-        pygame.draw.rect(game_window,white,self.rect)
-        pygame.display.update(self.rect)
-        
+#Grouping all sprites
+all_sprites = pygame.sprite.Group()
+#Adding sprites to group
+all_sprites.add(P1, O1, O2, O3, O4, O5)
 
-    #def is_hit(self):
-        #for event in pygame.event.get()
-            #if 
-player = Player()
+#Increase speed every 1 second, new User event
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
 
-
-
-#class object:
-    #def __init__(self):
-        #self.positionY = random
-        #self.positionX = random
-
-
-# Main Function
+#Game Loop
 while True:
-	
-    player.move()
 
-	# displaying score countinuously
+    #Cycle through all events occuring
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+        if event.type == INC_SPEED:
+            SPEED += 2
+
+    #Filling background
     game_window.fill(black)
 
-	# Refresh game screen
+    #Move P1
+    P1.move() 
+
+    #Move & Re-draws all Sprites
+    for entity in all_sprites:
+        game_window.blit(entity.surf, entity.rect)
+        entity.move()
+
+    #End game on collision
+    if pygame.sprite.spritecollideany(P1, objects):
+        game_over()
+
+    #Display score continuously
+    show_score(1, white, 'times new roman', 20)
+
+    #Refresh game screen
     pygame.display.update()
 
-	# Frame Per Second /Refresh Rat
+    #Frame Per Second /Refresh Rate
+    FramePerSec.tick(FPS)
 
-    
-#while True: # main game loop
-        #for event in pygame.event.get():
-            #if event.type == pygame.QUIT:
-                #pygame.quit()
-#sys.exit()
-#pygame.display.update()
+
+
+
+
+
+
+
 
---
+++ JupyterNotebooks/SemesterProject/README.md
@@ -0,0 +1,47 @@
+# Dodge Obsticle Game
+Game Summary:
+
+    Dodge Obsticle game is a single player game in which the objective is to avoide the incoming obsticles for as long as possible while stacking up  score. 5 objects unique in size, speed, & color constantly approach your player and it is up to the user to avoid these obsticles.
+
+Necessary Imports:
+
+    -import pygame
+    -from pygame.locals import *
+    -import time
+    -import random   
+
+Controls:
+
+    -Right arrow moves player right
+
+    -Left arrow moves player left
+
+    -Friction is involved when slowing down
+
+    -Brief acceleration period when key is held down
+
+    -Wrap around window function included
+
+Scoring Mechanism:
+    
+    -For every obsticle dodged, the user recieves 5 points
+
+    -How many points can you stack up?
+
+Game Over:
+
+    -When the player collides with any obsticle
+    
+    -Final Score is displayed
+
+Exit:
+
+    -3 seconds after collision, game automatically closes
+
+    -Option to exit mid game included via exit button located in window
+
+Notes:
+
+    -Notes included for each line of code in code file
+
+# Thank You For
